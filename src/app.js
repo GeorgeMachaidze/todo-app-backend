fe9652a038d7e700e9e44f7e48545c2789696911
Wakana <wakanichi13@gmail.com>
2023-07-26 16:47:28 +0400

    feat: test get request

--- src/app.js
+++ src/app.js
@@ -14,10 +14,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "app works!" });
-});
-
 app.get("/api/todos", getAllTodos);
 
 app.listen(3000);
